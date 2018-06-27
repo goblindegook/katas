43eb4b80ccce121ab24d9c5d54b279900f32e5d2
Luis Rodrigues <goblindegook@users.noreply.github.com>
2018-06-28 00:34:49 +0100

    Roman numerals kata

---
+++ typescript/src/roman-numerals/roman-numerals.ts
@@ -0,0 +1,36 @@
+const lookup: { readonly [symbol: string]: number } = {
+  IV: 4,
+  I: 1,
+  V: 5,
+  IX: 9,
+  X: 10,
+  XL: 40,
+  L: 50,
+  XC: 90,
+  C: 100,
+  CD: 400,
+  D: 500,
+  CM: 900,
+  M: 1000
+}
+
+function fail (): never {
+  throw new Error()
+}
+
+function assertValid (s: string, roman: string): void {
+  !roman.startsWith(s + s + s + s) && lookup[s] || fail()
+}
+
+function getNextSymbol ([s1, s2]: string): string {
+  return s2 && lookup[s1 + s2] ? s1 + s2 : s1
+}
+
+function _convert (acc: number, roman: string): number {
+  const s = getNextSymbol(roman)
+  return !s ? acc : assertValid(s, roman) || _convert(acc + lookup[s], roman.slice(s.length))
+}
+
+export function convert (roman: string): number {
+  return _convert(0, roman)
+}
