ed6b71921fd2d9ff3ada30c58f012d81485fa4b0
Luis Rodrigues <goblindegook@users.noreply.github.com>
2019-01-21 23:41:21 +0000

    Playing with elm-lang

---
+++ elm/fizzbuzz/src/Main.elm
@@ -0,0 +1,35 @@
+module Main exposing (fizzbuzz)
+
+
+range : Int -> Int -> List Int
+range a b =
+    if a < b then
+        List.range a b
+
+    else
+        List.range b a |> List.reverse
+
+
+isMultiple : Int -> Int -> Bool
+isMultiple m n =
+    remainderBy m n == 0
+
+
+convert : Int -> String
+convert n =
+    if isMultiple 15 n then
+        "fizzbuzz"
+
+    else if isMultiple 5 n then
+        "buzz"
+
+    else if isMultiple 3 n then
+        "fizz"
+
+    else
+        String.fromInt n
+
+
+fizzbuzz : Int -> Int -> List String
+fizzbuzz a b =
+    range a b |> List.map convert
---
+++ elm/fizzbuzz/tests/MainTests.elm
@@ -0,0 +1,28 @@
+module MainTests exposing (suite)
+
+import Expect exposing (Expectation)
+import Fuzz exposing (Fuzzer, int)
+import Main exposing (..)
+import Test exposing (..)
+
+
+suite : Test
+suite =
+    describe "Fizzbuzz"
+        [ test "multiples of 3 return fizz" <|
+            \_ -> fizzbuzz 3 3 |> Expect.equal [ "fizz" ]
+        , test "multiples of 5 return buzz" <|
+            \_ -> fizzbuzz 5 5 |> Expect.equal [ "buzz" ]
+        , test "multiples of 3 and 5 return buzz" <|
+            \_ -> fizzbuzz 15 15 |> Expect.equal [ "fizzbuzz" ]
+        , test "any other number is retained" <|
+            \_ -> fizzbuzz 1 1 |> Expect.equal [ "1" ]
+        , test "converts ascending ranges" <|
+            \_ -> fizzbuzz 2 3 |> Expect.equal [ "2", "fizz" ]
+        , test "converts descending ranges" <|
+            \_ -> fizzbuzz 3 2 |> Expect.equal [ "fizz", "2" ]
+        , fuzz int "multiples of 3 begin with fizz" <|
+            \m -> m * 3 |> (\n -> fizzbuzz n n |> List.head |> Maybe.withDefault "" |> String.left 4 |> Expect.equal "fizz")
+        , fuzz int "multiples of 5 end with buzz" <|
+            \m -> m * 5 |> (\n -> fizzbuzz n n |> List.head |> Maybe.withDefault "" |> String.right 4 |> Expect.equal "buzz")
+        ]
